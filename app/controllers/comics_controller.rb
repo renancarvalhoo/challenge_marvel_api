3557e1c45da2b4e8c808462ee4fb49a837fa074f
renan carvalho <renan.azevedodecarvalho@thetrainline.com>
2021-02-23 19:48:43 +0000

    Last adjusts on front end and controller, so seems almost finished
    Signed-off-by: renan carvalho <renan.azevedodecarvalho@thetrainline.com>

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -2,17 +2,18 @@ class ComicsController < ApplicationController
   LIMIT = 18
 
   def index
-    character = find_character(comics_params[:character_name])
+    @character_id = comics_params[:character_id] || find_character(comics_params[:character_name])
     comics_response = Marvel.comics(
       marvel_comic_parameters(
-        page_offset(comics_params[:page]),
+        comics_params[:page],
         comics_params[:orderBy] || '-onsaleDate',
-        character
+        @character_id
       )
     ).fetch
 
     @page = comics_params[:page].to_i || 0
     @total_pages = comics_response['total'] / LIMIT
+    ap @total_pages
     @favorites = Upvote.where(session_id: session.id.to_s).pluck(:comic_id)
     @comics = ComicsSerializer.new(comics_response['results']).serialize
   end
@@ -20,28 +21,28 @@ class ComicsController < ApplicationController
   private
 
   def comics_params
-    params.permit(:page, :orderBy, :character_name)
+    params.permit(:page, :orderBy, :character_name, :character_id)
   end
 
   def page_offset(page)
     (page || 0).to_i * LIMIT
   end
 
-  def marvel_comic_parameters(page, order_by, characters)
+  def marvel_comic_parameters(page, order_by, character)
     parameters = {
       offset: page_offset(page),
       limit: LIMIT,
       orderBy: order_by || '-onsaleDate'
     }
 
-    parameters.merge(characters: characters) if characters.any?
+    parameters = parameters.merge(characters: character) if character.present?
 
     parameters
   end
 
   def find_character(name)
-    character = Character.find_by('name = ?', "%#{name}%")
+    return unless name
 
-    character ? [character] : []
+    Character.find_by('name ilike ?', "%#{name}%")&.marvel_id&.to_s
   end
 end
--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -1,18 +1,18 @@
 class UpvotesController < ApplicationController
   def create
     Upvote.create(session_id: get_session, comic_id: permitted_params[:comic_id])
-    redirect_to root_path
+    redirect_to root_path(page: permitted_params[:page], character_id: permitted_params[:character_id])
   end
 
   def destroy
     Upvote.delete_by(session_id: get_session, comic_id: permitted_params[:id])
-    redirect_to root_path
+    redirect_to root_path(page: permitted_params[:page], character_id: permitted_params[:character_id])
   end
 
   private
 
   def permitted_params
-    params.permit(:id, :comic_id, :session_id)
+    params.permit(:id, :comic_id, :session_id, :page, :character_id)
   end
 
   def get_session
